7764ae21cf17bf49ce0057b30cf2e452478c7533
Kmet90 <kmet90@gmail.com>
2018-01-06 10:40:07 +0000

    Associative Arrays

---
+++ 08UserLogs/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08UserLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userIpMessageCount = new SortedDictionary<string, Dictionary<string, int>>();
+            var command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                GetUserIpMsgCount(userIpMessageCount, command);
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var user in userIpMessageCount)
+            {
+                Console.WriteLine($"{user.Key}:");
+
+                Console.Write(string.Join(", ",
+                    user.Value.Select(ipCount => ipCount.Key + " => " + ipCount.Value)));
+                Console.WriteLine(".");
+            }
+        }
+
+        static void GetUserIpMsgCount(
+            SortedDictionary<string, Dictionary<string, int>> userIpMessageCount,
+            string command)
+        {
+            var commandLineList = command.Split(' ').ToList();
+            var ipAddress = commandLineList[0].Substring(3);
+            var user = commandLineList[2].Substring(5);
+
+            if (!userIpMessageCount.ContainsKey(user))
+            {
+                userIpMessageCount[user] = new Dictionary<string, int>();
+            }
+            if (!userIpMessageCount[user].ContainsKey(ipAddress))
+            {
+                userIpMessageCount[user][ipAddress] = 0;
+            }
+            userIpMessageCount[user][ipAddress]++;
+        }
+    }
+}
